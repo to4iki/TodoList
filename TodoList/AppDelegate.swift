49322ac9c279e83ba0c09d2890cde0ea289c76f6
tsk takezawa <tsk.take815@gmail.com>
2016-02-23 00:03:03 +0900

    Initial Commit

---
+++ TodoList/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TodoList
+//
+//  Created by to4iki on 2/23/16.
+//  Copyright © 2016 to4iki. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
---
+++ TodoList/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by to4iki on 2/23/16.
+//  Copyright © 2016 to4iki. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
